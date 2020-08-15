7a1616716e7d44e841e32e5dd6d2cdf09b363258
Patrick Sturm <psturm-swift@e.mail.de>
2020-08-15 13:08:11 +0200

    Update test case manifest

--- Tests/PubFactoryTests/XCTestManifests.swift
+++ Tests/PubFactoryTests/XCTestManifests.swift
@@ -3,6 +3,9 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
+        testCase(ClosureProducerTests.allTests),
+        testCase(LockableTests.allTests),
+        testCase(ProducerTests.allTests),
     ]
 }
 #endif
